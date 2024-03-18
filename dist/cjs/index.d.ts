216b05001911a425cf7aac831d3b07db4ee9b586
Chris Chudzicki <christopher.chudzicki@gmail.com>
2024-03-18 15:22:27 -0400

    commit generated

---
+++ dist/cjs/index.d.ts
@@ -0,0 +1,3 @@
+export * as v0 from "./v0";
+export * as v1 from "./v1";
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/esm/index.js
@@ -0,0 +1,6 @@
+console.log("hello from @mitodl/open-api-client/dist/esm/index.js");
+import * as v0_1 from "./v0";
+export { v0_1 as v0 };
+import * as v1_1 from "./v1";
+export { v1_1 as v1 };
+//# sourceMappingURL=index.js.map
